e89c59e60991c4299a827b993c49bcdf6c8c08c4
Ilya Kolodnik <ilya.kolodnik@gmail.com>
2018-03-09 21:02:51 +0300

    vec3 class

--- 01_ppm_output.go
+++ 01_ppm_output.go
@@ -8,12 +8,10 @@ func main() {
 	fmt.Printf("P3\n%v %v\n255\n", nx, ny)
 	for j := ny - 1; j >= 0; j-- {
 		for i := 0; i < nx; i++ {
-			r := float32(i) / float32(nx)
-			g := float32(j) / float32(ny)
-			b := 0.2
-			ir := int(255.99 * r)
-			ig := int(255.99 * g)
-			ib := int(255.99 * b)
+			col := vec3{float32(i) / float32(nx), float32(j) / float32(ny), 0.2}
+			ir := int(255.99 * col.r())
+			ig := int(255.99 * col.g())
+			ib := int(255.99 * col.b())
 			fmt.Printf("%v %v %v\n", ir, ig, ib)
 		}
 	}
---
+++ 02_vec3.go
@@ -0,0 +1,77 @@
+package main
+
+import "math"
+
+type vec3 struct {
+	e0, e1, e2 float32
+}
+
+func (vec vec3) x() float32 {
+	return vec.e0
+}
+
+func (vec vec3) y() float32 {
+	return vec.e1
+}
+
+func (vec vec3) z() float32 {
+	return vec.e2
+}
+
+func (vec vec3) r() float32 {
+	return vec.e0
+}
+
+func (vec vec3) g() float32 {
+	return vec.e1
+}
+
+func (vec vec3) b() float32 {
+	return vec.e2
+}
+
+func (vec vec3) squared_length() float32 {
+	return float32(vec.e0*vec.e0 + vec.e1*vec.e1 + vec.e2*vec.e2)
+}
+
+func (vec vec3) length() float32 {
+	return float32(math.Sqrt(float64(vec.squared_length())))
+}
+
+func (v1 vec3) plus(v2 vec3) vec3 {
+	return vec3{v1.e0 + v2.e0, v1.e1 + v2.e1, v1.e2 + v2.e2}
+}
+
+func (v1 vec3) minus(v2 vec3) vec3 {
+	return vec3{v1.e0 - v2.e0, v1.e1 - v2.e1, v1.e2 - v2.e2}
+}
+
+func (v1 vec3) multiply(v2 vec3) vec3 {
+	return vec3{v1.e0 * v2.e0, v1.e1 * v2.e1, v1.e2 * v2.e2}
+}
+
+func (v1 vec3) divide(v2 vec3) vec3 {
+	return vec3{v1.e0 / v2.e0, v1.e1 / v2.e1, v1.e2 / v2.e2}
+}
+
+func (v1 vec3) multiply_by(f float32) vec3 {
+	return vec3{v1.e0 * f, v1.e1 * f, v1.e2 * f}
+}
+
+func (v1 vec3) divide_by(f float32) vec3 {
+	return vec3{v1.e0 / f, v1.e1 / f, v1.e2 / f}
+}
+
+func (v1 vec3) dot(v2 vec3) float32 {
+	return v1.e0*v2.e0 + v1.e1*v2.e1 + v1.e2*v2.e2
+}
+
+func (v1 vec3) cros(v2 vec3) vec3 {
+	return vec3{v1.e1*v2.e2 - v1.e2*v2.e1,
+		-(v1.e0*v2.e2 - v1.e2*v2.e0),
+		v1.e0*v2.e1 - v1.e1*v2.e0}
+}
+
+func (v1 vec3) unit_vector() vec3 {
+	return v1.divide_by(v1.length())
+}
