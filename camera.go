a522bca691b65a58afd57d58d5c0b3b75b9c074a
Ilya Kolodnik <ilya.kolodnik@gmail.com>
2018-03-31 12:59:07 +0300

    chapter 11: defocus blur

--- camera.go
+++ camera.go
@@ -1,26 +1,48 @@
 package main
 
-import "math"
+import (
+	"math"
+	"math/rand"
+)
 
 type camera struct {
-	origin, lowerLeftCorner, horizontal, vertical vec3
+	origin, lowerLeftCorner, horizontal, vertical, u, v, w vec3
+	lensRadius                                             float32
 }
 
 func (c camera) getRay(u float32, v float32) ray {
-	return ray{c.origin, c.lowerLeftCorner.plus(c.horizontal.multiplyBy(u)).plus(c.vertical.multiplyBy(v).minus(c.origin))}
+	rd := randomInUnitDisk().multiplyBy(c.lensRadius)
+	offset := c.u.multiplyBy(rd.x()).plus(c.v.multiplyBy(rd.y()))
+	return ray{c.origin.plus(offset), c.lowerLeftCorner.plus(c.horizontal.multiplyBy(u)).plus(c.vertical.multiplyBy(v).minus(c.origin)).minus(offset)}
 }
 
-func defaultCamera(lookfrom vec3, lookat vec3, vup vec3, vfov float32, aspect float32) *camera {
+func defaultCamera(lookfrom vec3, lookat vec3, vup vec3, vfov float32, aspect float32, aperture float32, focusDist float32) *camera {
+	lensRadius := aperture / 2.0
 	theta := vfov * math.Pi / 180.0
 	halfHeight := float32(math.Tan(float64(theta / 2.0)))
 	halfWidth := aspect * halfHeight
 	w := lookfrom.minus(lookat).unitVector()
 	u := vup.cross(w).unitVector()
 	v := w.cross(u)
-	llCorner := lookfrom.minus(u.multiplyBy(halfWidth)).minus(v.multiplyBy(halfHeight)).minus(w)
+	llCorner := lookfrom.minus(u.multiplyBy(halfWidth * focusDist)).minus(v.multiplyBy(halfHeight * focusDist)).minus(w.multiplyBy(focusDist))
 	return &camera{
 		origin:          lookfrom,
 		lowerLeftCorner: llCorner,
-		horizontal:      u.multiplyBy(2.0 * halfWidth),
-		vertical:        v.multiplyBy(2.0 * halfHeight)}
+		horizontal:      u.multiplyBy(2.0 * halfWidth * focusDist),
+		vertical:        v.multiplyBy(2.0 * halfHeight * focusDist),
+		u:               u,
+		v:               v,
+		w:               w,
+		lensRadius:      lensRadius}
+}
+
+func randomInUnitDisk() vec3 {
+	var p vec3
+	for {
+		p = vec3{rand.Float32(), rand.Float32(), 0}.multiplyBy(2.0).minus(vec3{1.0, 1.0, 0.0})
+		if p.dot(p) < 1.0 {
+			break
+		}
+	}
+	return p
 }
